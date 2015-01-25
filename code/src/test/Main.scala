51dc40e5ef2c231c6709a6ce6e5eb8d5e2e1deed
Yanlin Wang <huohuohuomumu@gmail.com>
2015-01-25 21:05:43 +0800

    trying merge Eval and Print using complex version library. [ClassCastException]

---
+++ code/src/test/Main.scala
@@ -0,0 +1,73 @@
+package test
+
+import core.Algebras._
+import core.Exp._
+
+object Exp {
+  
+  /*
+   * StackAlg -> ExpAlg
+   * SelfStackAlg -> SelfExpAlg
+   */
+  trait SelfExpAlg[S <: E, E] extends ExpAlg[S,Open[S, E]] with SelfFAlg[ExpAlg,S,E]  
+  
+  /*
+   * OpenStackAlg -> OpenExpAlg
+   */
+  type OpenExpAlg[S <: E, E] = (=> ExpAlg[S,S]) => ExpAlg[S,Open[S, E]]
+  
+  /* EF : Eval Feature
+   * SF -> EF
+   * stack() -> Lit() and Add()
+   * SelfStackAlg -> SelfEvalAlg
+   */
+  trait EF[S <: IEval] extends SelfExpAlg[S, IEval] { 
+    def Lit(x:Int) = self => new IEval() { def eval = x }
+    def Add(e1:S,e2:S) = self => new IEval() { def eval = e1.eval + e2.eval }
+  }
+  def ef[S <: IEval] : OpenExpAlg[S,IEval] = 
+    s => new EF[S] { lazy val fself = s }
+    
+  /* PF : Print Feature */  
+  trait PF[S <: IPrint] extends SelfExpAlg[S, IPrint] { 
+    def Lit(x:Int) = self => new IPrint() { def print = x.toString }
+    def Add(e1:S,e2:S) = self => new IPrint() { def print = "(" + e1.print + "+" + e2.print + ")" }
+  }
+  def pf[S <: IPrint] : OpenExpAlg[S,IPrint] = 
+    s => new PF[S] { lazy val fself = s }
+  
+  
+}
+
+object ExpComb extends AlgebraDefault[ExpAlg] 
+
+object Main extends App {  
+  import Exp._
+  import ExpComb._
+  
+  def exp[E](alg : ExpAlg[E, E]) = alg.Add(alg.Lit(3), alg.Lit(4))
+  
+  override def main(args: Array[String]) {
+    println(">>> Eval Feature")
+    testEval()
+    println(">>> Print Feature")
+    testPrint()
+    
+    // TODO : error test, combine eval and print feature  
+    println(">>> Eval + Print")
+    testEP()
+  }
+  
+  def testEval() = {
+    val o = exp[IEval](fclose(ef))
+    println(o.eval)
+  }
+  def testPrint() = {
+    val o = exp[IPrint](fclose(pf))
+    println(o.print)
+  }
+  def testEP() = {
+    val o = exp[IEval with IPrint](fclose(combine[IEval,IPrint,IEval with IPrint](ef, pf)))
+    println("Eval: " + o.eval + "\nPrint: " + o.print)
+  }
+}
